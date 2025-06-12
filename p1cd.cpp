1bf917ef0ce9c9ba317bc3be43df74e850e70e9c
Abdul Lukmana <154068062+abdullukmana@users.noreply.github.com>
2025-06-12 13:48:27 +0700

    Update p1cd.cpp

--- p1cd.cpp
+++ p1cd.cpp
@@ -3,36 +3,42 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
-using namespace cv;
 
-void Invert(Mat& src, Mat& dst);
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    cv::Mat img = cv::imread("D:/Praktikum Pencitraan Digital/Images/michael.jpg");
-    cv::Mat inverted_image;
+    // Membaca gambar dari file
+    Mat img = imread("D:/Praktikum Pencitraan Digital/Images/michael.jpg");
+    Mat inverted_image;
 
-    if (img.empty())
-        std::cout << "failed to open img.jpg" << std::endl;
-    else
-        std::cout << "img.jpg loaded OK" << std::endl;
+    if (img.empty()) {
+        cout << "Failed to open michael.jpg" << endl;
+        return -1;
+    } else {
+        cout << "michael.jpg loaded OK" << endl;
+    }
 
-    namedWindow("First OpenCV Application", WINDOW_NORMAL /*WINDOW_AUTOSIZE*/);
-    cv::imshow("First OpenCV Application", img);
+    // Menampilkan gambar asli
+    namedWindow("First OpenCV Application", WINDOW_NORMAL);
+    imshow("First OpenCV Application", img);
 
-    // konversi ke invert/negatif
-    // Invert(img, result);
+    // Konversi ke gambar negatif/inversi
     bitwise_not(img, inverted_image);
 
-    namedWindow("Invert Image", WINDOW_NORMAL /*WINDOW_AUTOSIZE*/);
-    cv::imshow("Invert Image", inverted_image);
+    // Menampilkan gambar hasil inversi
+    namedWindow("Invert Image", WINDOW_NORMAL);
+    imshow("Invert Image", inverted_image);
 
-    // Menyimpan file invert
-    cv::imwrite("D:\\private\\ReuniITBS2-20221206T021533Z-001\\invert.JPG", inverted_image);
+    // Menyimpan hasil citra invert
+    imwrite("D:/Praktikum Pencitraan Digital/Images/michael_invert.jpg", inverted_image);
 
-    cv::moveWindow("First OpenCV Application", 0, 45);
-    cv::waitKey(0);
-    cv::destroyAllWindows();
+    // Mengatur posisi jendela
+    moveWindow("First OpenCV Application", 0, 45);
+
+    waitKey(0);
+    destroyAllWindows();
 
     return 0;
 }
