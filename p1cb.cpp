bff4985a4b1bee1070c78a4243e1ba5aee924115
Abdul Lukmana <154068062+abdullukmana@users.noreply.github.com>
2025-06-12 13:44:45 +0700

    Update p1cb.cpp

--- p1cb.cpp
+++ p1cb.cpp
@@ -1,44 +1,48 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
+
 using namespace cv;
+using namespace std;
 
 int main()
 {
-    cv::Mat img = cv::imread("D:/Praktikum Pencitraan Digital/Images/michael.jpg");
-    cv::Mat gray, biner;
+    int imgcols, imgrows, numimgpixels, dimension, type, sizeWidth, sizeHeight;
 
-    if (img.empty())
-        std::cout << "failed to open img.jpg" << std::endl;
-    else
-        std::cout << "img.jpg loaded OK" << std::endl;
+    // Membaca gambar
+    Mat img = imread("D:/Praktikum Pencitraan Digital/Images/michael.jpg");
+    Mat result;
 
-    namedWindow("First OpenCV Application", WINDOW_NORMAL /*WINDOW_AUTOSIZE*/);
-    cv::imshow("First OpenCV Application", img);
+    if (img.empty()) {
+        cout << "Failed to open michael.jpg" << endl;
+    }
+    else {
+        cout << "michael.jpg loaded OK" << endl;
+    }
 
-    // konversi dari truecolor ke grayscale
-    cvtColor(img, gray, COLOR_BGR2GRAY);
-    // Menampilkan
-    namedWindow("Grayscale", WINDOW_NORMAL /*WINDOW_AUTOSIZE*/);
-    cv::imshow("Grayscale", gray);
-    // Menyimpan file grayscale
-    cv::imwrite("D:\\private\\ReuniTBS2-20221206T021533Z-001\\gray.JPG", gray);
+    namedWindow("First OpenCV Application", WINDOW_NORMAL);
+    imshow("First OpenCV Application", img);
+    moveWindow("First OpenCV Application", 0, 45);
 
-    // konversi grayscale ke biner
-    threshold(gray, biner, 100, 255, THRESH_BINARY);
-    // Menampilkan
-    namedWindow("Binary image", WINDOW_NORMAL /*WINDOW_AUTOSIZE*/);
-    cv::imshow("Binary image", biner);
-    // Menyimpan file grayscale
-    cv::imwrite("D:\\private\\ReuniTBS2-20221206T021533Z-001\\gray.JPG", biner);
+    // Informasi citra
+    imgcols = img.cols;
+    imgrows = img.rows;
+    numimgpixels = imgcols * imgrows;
+    sizeWidth = img.size().width;
+    sizeHeight = img.size().height;
 
-    cv::moveWindow("First OpenCV Application", 0, 45);
-    cv::moveWindow("Grayscale", 500, 45);
-    cv::moveWindow("Binary image", 250, 90);
-    cv::waitKey(0);
-    cv::destroyAllWindows();
+    cout << "Informasi citra\n";
+    cout << "Jumlah kolom citra     = " << imgcols << "\n";
+    cout << "Jumlah baris citra     = " << imgrows << "\n";
+    cout << "Lebar citra            = " << sizeWidth << "\n";
+    cout << "Tinggi citra           = " << sizeHeight << "\n";
+    cout << "Ukuran matriks citra   = " << imgrows << " x " << imgcols << "\n";
+    cout << "Jumlah pixel citra     = " << numimgpixels << "\n";
+
+    waitKey(0);
+    destroyAllWindows();
 
     return 0;
 }
