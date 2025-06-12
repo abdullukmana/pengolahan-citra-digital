4826e8985d7ec18cdf075cc0795f852b0257a303
Abdul Lukmana <154068062+abdullukmana@users.noreply.github.com>
2025-06-12 14:10:33 +0700

    Update p1cj.cpp

--- p1cj.cpp
+++ p1cj.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include "opencv2/imgproc.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace std;
 using namespace cv;
@@ -19,16 +19,16 @@ int main()
     }
 
     // Define sharpening kernel
-    Mat sharpeningKernel = (Mat_<double>(3, 3) <<
-        0, -1, 0,
-        -1, 5, -1,
-        0, -1, 0);
+    Mat sharpeningKernel = (Mat_<double>(3, 3) << 
+         0, -1,  0,
+        -1,  5, -1,
+         0, -1,  0);
 
     Mat output;
 
     // filter2D is used to perform the convolution.
-    // The third parameter (depth) is set to -1 which means the bit-depth of the output image is the
-    // same as the input image. So if the input image is of type CV_8UC3, the output image will also be of the same type.
+    // The third parameter (depth) is set to -1 which means the bit-depth of the output image is the same as the input image.
+    // So if the input image is of type CV_8UC3, the output image will also be of the same type.
     filter2D(image, output, -1, sharpeningKernel);
 
     // Create windows to display images
